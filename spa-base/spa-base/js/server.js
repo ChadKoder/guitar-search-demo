20cfac5114109cf3fb9dcf5daf8ab57e68f93972
Chad Keibler <chad.keibler@aesynt.com>
2015-10-10 16:44:52 -0400

    adding some test stuff

--- spa-base/spa-base/js/server.js
+++ spa-base/spa-base/js/server.js
@@ -11,49 +11,6 @@ var mongoClient = require('mongodb').MongoClient;
 var db = require(path.resolve('../app/config/db'));
 var port = process.env.PORT || 8080;
 
-//TODO: DELETE THIS BELOW
-var ObjectId = require('mongodb').ObjectID;
-var insertDocument = function(db, callback) {
-    db.collection('restaurants').insertOne( {
-        "address" : {
-            "street" : "2 Avenue",
-            "zipcode" : "10075",
-            "building" : "1480",
-            "coord" : [ -73.9557413, 40.7720266 ]
-        },
-        "borough" : "Manhattan",
-        "cuisine" : "Italian",
-        "grades" : [
-           {
-               "date" : new Date("2014-10-01T00:00:00Z"),
-               "grade" : "A",
-               "score" : 11
-           },
-           {
-               "date" : new Date("2014-01-16T00:00:00Z"),
-               "grade" : "B",
-               "score" : 17
-           }
-        ],
-        "name" : "Vella",
-        "restaurant_id" : "41704620"
-    }, function(err, result) {
-        assert.equal(err, null);
-        console.log("Inserted a document into the restaurants collection.");
-        callback(result);
-    });
-};
-
-//Call the insertDocument function.
-mongoClient.connect(db.url, function(err, db) {
-    assert.equal(null, err);
-    insertDocument(db, function() {
-        db.close();
-    });
-});
-    //TODO: DELETE ABOVE TEST CODE
-
-
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
---
+++ spa-base/spa-base/test-service-remove-me/db-test-conn.js
@@ -0,0 +1,87 @@
+﻿
+//TODO: DELETE THIS BELOW
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+var ObjectId = require('mongodb').ObjectID;
+var insertDocument = function (db, callback) {
+    db.collection('restaurants').insertOne({
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "grades": [
+           {
+               "date": new Date("2014-10-01T00:00:00Z"),
+               "grade": "A",
+               "score": 11
+           },
+           {
+               "date": new Date("2014-01-16T00:00:00Z"),
+               "grade": "B",
+               "score": 17
+           }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    }, function (err, result) {
+        assert.equal(err, null);
+        console.log("Inserted a document into the restaurants collection.");
+        callback(result);
+    });
+};
+
+//Call the insertDocument function.
+//mongoClient.connect(db.url, function(err, db) {
+//    assert.equal(null, err);
+//    insertDocument(db, function() {
+//        db.close();
+//    });
+//});
+
+var findRestaurants = function (db, callback) {
+    var cursor = db.collection('restaurants').find();
+    cursor.each(function (err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            console.dir(doc);
+        } else {
+            callback();
+        }
+    });
+};
+
+MongoClient.connect(db.url, function (err, db) {
+    assert.equal(null, err);
+    console.log('begin retrieving restaurants...');
+    findRestaurants(db, function () {
+        db.close();
+    });
+    console.log('end retrieving restaurants...');
+});
+
+var removeRestaurants = function (db, callback) {
+    db.collection('restaurants').deleteMany(
+       { "borough": "Manhattan" },
+       function (err, results) {
+           console.log(results);
+           callback();
+       }
+    );
+};
+
+//mongoClient.connect(db.url, function (err, db) {
+//    assert.equal(null, err);
+//    console.log('Deleting restaurants');
+//    removeRestaurants(db, function () {
+//        db.close();
+//    });
+//});
+//TODO: DELETE ABOVE TEST CODE
