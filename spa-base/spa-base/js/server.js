179f2f387c20d4934f0b137b66ace25e963f0da7
Chad Keibler <chad.keibler@aesynt.com>
2015-10-10 14:38:49 -0400

    updated packges

--- spa-base/spa-base/js/server.js
+++ spa-base/spa-base/js/server.js
@@ -44,33 +44,34 @@ console.log('Magic happens on port ' + port);
 // expose app
 exports = module.exports = app;
 
-//var Db = require('mongodb').Db,
-//    MongoClient = require('mongodb').MongoClient,
-//    Server = require('mongodb').Server,
-//    ReplSetServers = require('mongodb').ReplSetServers,
-//    ObjectID = require('mongodb').ObjectID,
-//    Binary = require('mongodb').Binary,
-//    GridStore = require('mongodb').GridStore,
-//    Code = require('mongodb').Code,
-//    BSON = require('mongodb').pure().BSON,
-//    assert = require('assert');
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Code = require('mongodb').Code,
+   // BSON = require('mongodb').pure().BSON,
+    assert = require('assert');
 
-//var db = new Db('integration_tests', new Server("127.0.0.1", 27017,
-// { auto_reconnect: false, poolSize: 4 }), { w: 0, native_parser: false });
+var db = new Db('integration_tests', new Server("127.0.0.1", 27017,
+ { auto_reconnect: false, poolSize: 4 }), { w: 0, native_parser: false });
 
-//// Establish connection to db
-//db.open(function (err, db) {
-//    assert.equal(null, err);
-//    console.log('connected to the db');
-//    //// Add a user to the database
-//    //db.addUser('user', 'name', function (err, result) {
-//    //    assert.equal(null, err);
+// Establish connection to db
+db.open(function (err, db) {
+    assert.equal(null, err);
+    console.log('connected to the db');
+    
+    //// Add a user to the database
+    //db.addUser('user', 'name', function (err, result) {
+    //    assert.equal(null, err);
 
-//    //    // Authenticate
-//    //    db.authenticate('user', 'name', function (err, result) {
-//    //        assert.equal(true, result);
+    //    // Authenticate
+    //    db.authenticate('user', 'name', function (err, result) {
+    //        assert.equal(true, result);
 
-//    //        db.close();
-//    //    });
-//    //});
-//});
+    //        db.close();
+    //    });
+    //});
+});
