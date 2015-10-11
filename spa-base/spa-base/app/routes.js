e2e7cc7061ec00bbdc20701d3ae814bf4f0dd8b5
Chad Keibler <chad.keibler@aesynt.com>
2015-10-11 16:04:09 -0400

    updated to be more close to example

--- spa-base/spa-base/app/routes.js
+++ spa-base/spa-base/app/routes.js
@@ -1,20 +1,6 @@
-﻿// app/routes.js
-var express = require('express');
-var app = express();
-
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/bower_components', express.static(__dirname + '/../bower_components'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/partials', express.static(__dirname + '/partials'));
-
-app.all('/*', function (req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: __dirname });
-});
-
-
-// grab the nerd model we just created
-var Nerd = require('../models/nerd');
+﻿// grab the nerd model we just created
+var path = require('path');
+var Nerd = require(path.resolve('../models/nerd'));
 
 module.exports = function (app) {
 
@@ -23,14 +9,14 @@ module.exports = function (app) {
     // authentication routes
 
     // sample api route
-    app.get('/api/nerds', function (req, res) {
+    app.get(path.resolve('/api/nerds'), function (req, res) {
         // use mongoose to get all nerds in the database
         Nerd.find(function (err, nerds) {
+
             // if there is an error retrieving, send the error. 
             // nothing after res.send(err) will execute
-            if (err) {
+            if (err)
                 res.send(err);
-            }
 
             res.json(nerds); // return all nerds in JSON format
         });
@@ -43,8 +29,8 @@ module.exports = function (app) {
     // route to handle all angular requests
     app.get('*', function (req, res) {
         var path = require('path');
-        res.sendfile(path.resolve('../public/views/index.html')); // load our public/index.html file
-        //res.sendFile(path.join(__dirname, '../public/views', 'index.html'));
+
+        res.sendFile(path.resolve('../public/views/index.html')); // load our public/index.html file
     });
 
 };
