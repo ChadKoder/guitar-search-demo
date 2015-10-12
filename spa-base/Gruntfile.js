fd87c872c086b3b5ec5364442d58382370c7f574
Chad Keibler <chad.keibler@aesynt.com>
2015-10-12 18:21:18 -0400

    updating for jasmine and stuff

--- spa-base/Gruntfile.js
+++ spa-base/Gruntfile.js
@@ -8,7 +8,8 @@ module.exports = function(grunt) {
       },
       dist: {
         //src: ['public/js/*.js'],
-		src: ['public/libs/angular/*.js', 'public/libs/angular-route/*.js', 'public/libs/bootstrap/*.js'],			 
+		//src: ['public/libs/**/*.js', 'public/js/**/*.js', 'public/libs/angular-route/*.js', 'public/libs/bootstrap/*.js'],			 
+		src: ['public/libs/angular/*.js', 'public/libs/angular-route/*.js', 'public/js/**/*.js', 'public/libs/angular-route/*.js', 'public/libs/bootstrap/*.js'],			 
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
@@ -27,7 +28,7 @@ module.exports = function(grunt) {
   //  },
     jshint: {
       //files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-	  files: ['Gruntfile.js', 'public/js/*.js', 'public/js/controllers/*.js', 'public/js/services/*.js'],
+	  files: ['Gruntfile.js', 'public/js/**/*.js'],//, 'public/js/controllers/*.js', 'public/js/services/*.js'],	  
       options: {
         // options here to override JSHint defaults
         globals: {
@@ -38,21 +39,41 @@ module.exports = function(grunt) {
         }
       }
     },
+	//jasmine: {
+		 // Your project's source files
+      //src : ['public/libs/angular/*.js', 'public/libs/angular-route/*.js', 'public/js/**/*.js'],
+	//  src : ['public/libs/angular/*.js'],
+      // Your Jasmine spec files
+  //    specs : 'src/tests/**/*spec.js',
+	  //vendor: [
+                //'public//dist/jquery.js'                
+            //]
+      // Your spec helper files
+    //  helpers : 'specs/helpers/*.js'	  
+	//},
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']//, 'qunit']
-    }
+    }	
   });
+
+  //if (grunt.option('debug')){
+	//  console.log(grunt.config('jshint.files'));
+  //}
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
  // grunt.loadNpmTasks('grunt-contrib-qunit');
+ //grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat'); 
+  
+   //grunt.registerTask('default', 'jasmine');
 
-  grunt.registerTask('test', ['jshint']);//, 'qunit']);
+ // grunt.registerTask('test', ['jshint']);//, 'qunit']);
 
   //grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  //grunt.registerTask('default', ['jshint', 'concat', 'jasmine', 'uglify']);
   grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
 };
