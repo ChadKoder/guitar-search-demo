39e5bce67c0e2ed1a43d1fcd4026c11b32cb1867
Chad Keibler <chad.keibler@aesynt.com>
2015-10-17 07:03:16 -0400

    fixed injection of searchService in SearchCtrl

--- job-search/public/js/controllers/SearchCtrl.js
+++ job-search/public/js/controllers/SearchCtrl.js
@@ -1,4 +1,4 @@
 ﻿//public/js/controllers/SearchCtrl
-angular.module('SearchCtrl', []).controller('SearchCtrl', ['searchService', function ($scope, searchService) {
+angular.module('SearchCtrl', []).controller('SearchCtrl', ['$scope', 'searchService', function ($scope, searchService) {
     $scope.tagline = searchService.get('another test!');
 }]);
