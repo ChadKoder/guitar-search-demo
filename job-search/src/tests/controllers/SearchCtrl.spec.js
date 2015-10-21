e46c731ff79e54315c4d36372db1f4c55f3e2ad4
Chad Keibler <chad.keibler@aesynt.com>
2015-10-20 21:02:05 -0400

    removed old commented out tests

--- job-search/src/tests/controllers/SearchCtrl.spec.js
+++ job-search/src/tests/controllers/SearchCtrl.spec.js
@@ -129,34 +129,4 @@
             expect($scope.searchError).toBe(true);
         });
     });
-    //describe('$scope.search()', function () {
-    //    beforeEach(function () {
-    //        spyOn(ctrl, 'getSearchResultsSuccess');//.andCallThrough();
-    //        spyOn(ctrl, 'getSearchResultsFailure'); //.andCallThrough();
-    //        $scope.searchText = 'findJobs';
-    //        $scope.search();
-    //        $rootScope.$digest();
-    //        //$scope.$apply();
-    //    });
-
-    //    //it('should call ctrl.getSearchResultsSuccess', function() {
-    //    //    expect(ctrl.getSearchResultsSuccess).toHaveBeenCalled();
-    //    //});
-
-    //    it('should call authenticService.getSearchResults', function () {
-    //        // expect(service.getSearchResults).toHaveBeenCalled();
-    //        expect(1).toEqual(1);
-    //    });
-    //});
-
-    //describe('ctrl.getSearchResultsSuccess', function () {
-    //    beforeEach(function () {
-    //        var searchResults = 'search-results';
-    //        ctrl.getSearchResultsSuccess(searchResults);
-    //    });
-
-    //    it('should set $scope.allListings', function () {
-    //        expect($scope.allListings).toBe('search-results');
-    //    });
-    //});
 });
