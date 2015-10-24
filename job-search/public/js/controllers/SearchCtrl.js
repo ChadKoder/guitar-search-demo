3173b4e6621d84e3a1dc76aa3fbb83c8303bb93f
Chad Keibler <chad.keibler@aesynt.com>
2015-10-23 20:22:32 -0400

    refactored some things in ctrl. still needs some work

--- job-search/public/js/controllers/SearchCtrl.js
+++ job-search/public/js/controllers/SearchCtrl.js
@@ -12,41 +12,81 @@ angular.module('SearchCtrl', []).controller('SearchCtrl', ['$scope', '$http', '$
             .then(ctrl.loadSuccess, ctrl.loadFailure);
     };
 
-    ctrl.combineResults = function(authenticResults, gitHubResults, usaJobsResults) {
+    ctrl.combineResults = function (data) {
+        var authenticData = data[0].data.listings.listing;
+        var gitHubData = data[1].data;
+        var usaJobsData = data[2].data.JobData;
+
+        var authenticResults = ctrl.buildAuthenticResults(authenticData);
+        var gitHubResults = ctrl.buildGitHubResults(gitHubData);
+        var usaJobsResults = ctrl.buildUsaJobsResults(usaJobsData);
+
         var results = [];
+        results.push.apply(results, authenticResults);
+        results.push.apply(results, gitHubResults);
+        results.push.apply(results, usaJobsResults);
         
-        for (var i = 0; i < authenticResults.length; i++) {
-            results.push({
-                title: authenticResults[i].title + ' (AuthenticJobs Listing)',
-                description: authenticResults[i].description
-            });
-        }
-
-        for (var j = 0; j < gitHubResults.length; j++) {
-            results.push({
-                title: gitHubResults[j].title + ' (GitHub Listing)',
-                description: gitHubResults[j].description
-            });
-        }
-
-        for (var k = 0; k < usaJobsResults.length; k++) {
-            results.push({
-                title: usaJobsResults[k].JobTitle + ' (USAJobs Listing)',
-                description: usaJobsResults[k].JobSummary
-            });
-        }
-
         return results;
+    };
+
+    ctrl.buildGitHubResults = function (results) {
+        var gitHubResults = [];
+
+        if (_.isEmpty(results)) {
+            console.log('no search results found from git hub');
+            gitHubResults.length = 0;
+            return gitHubResults;
+        }
+
+        for (var j = 0; j < results.length; j++) {
+            gitHubResults.push({
+                title: results[j].title + ' (GitHub Listing)',
+                description: results[j].description
+            });
+        }
+        
+        return gitHubResults;
+    };
+    
+    ctrl.buildUsaJobsResults = function (results) {
+        var usaJobsResults = [];
+        if (_.isEmpty(results)) {
+            console.log('no search results found from usa jobs');
+            usaJobsResults.length = 0;
+            return usaJobsResults;
+        }
+
+        for (var i = 0; i < results.length; i++) {
+            usaJobsResults.push({
+                title: results[i].JobTitle + ' (USAJobs Listing)',
+                description: results[i].JobSummary
+            });
+        }
+
+        return usaJobsResults;
+    };
+    
+    ctrl.buildAuthenticResults = function (results) {
+        var authResults = [];
+        if (_.isEmpty(results)) {
+            console.log('no search results found from authentic jobs');
+            authResults.length = 0;
+            return authResults;
+        }
+        
+        for (var i = 0; i < results.length; i++) {
+            authResults.push({
+                title: results[i].title + ' (AuthenticJobs Listing)',
+                description: results[i].description
+            });
+        }
+
+        return authResults;
     };
     
     ctrl.loadSuccess = function (results) {
-            var authenticResults = results[0].data.listings.listing;
-            var gitHubResults = results[1].data;
-            var usaJobsResults = results[2].data.JobData;
-
-            var allResults = ctrl.combineResults(authenticResults, gitHubResults, usaJobsResults);
-
-            $scope.allListings = allResults;
+        var allResults = ctrl.combineResults(results);
+        $scope.allListings = allResults;
     };
 
     ctrl.loadFailure = function () {
