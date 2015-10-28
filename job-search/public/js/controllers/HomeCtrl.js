8cbca423527b9fc359569cd311659d62908ca133
Chad Keibler <chad.keibler@aesynt.com>
2015-10-27 21:22:08 -0400

    added delete button to favorites page

--- job-search/public/js/controllers/HomeCtrl.js
+++ job-search/public/js/controllers/HomeCtrl.js
@@ -1,6 +1,5 @@
 ﻿//js/controllers/HomeCtrl.js
 angular.module('HomeCtrl', []).controller('HomeCtrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
-
     var ctrl = this;
 
     ctrl.retrieveSavedJobs = function() {
@@ -15,6 +14,17 @@ angular.module('HomeCtrl', []).controller('HomeCtrl', ['$scope', '$http', '$sce'
             }
 
             $scope.jobs = cleanJobs;
+        });
+    };
+
+    $scope.delete = function (job) {
+        alert(job.title);
+        ctrl.removeFavoriteJob(job);
+    };
+
+    ctrl.removeFavoriteJob = function (job) {
+        $http.put('/update', { title: job.title }).success(function () {
+            console.log('job successfully removed');
         });
     };
 
