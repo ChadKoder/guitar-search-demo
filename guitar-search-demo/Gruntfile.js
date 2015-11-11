864c20e58e9d4e250bc12b99bf0b61ad2c68d8ca
Chad Keibler <chad.keibler@aesynt.com>
2015-11-11 15:46:57 -0500

    added dev task, fixed watch so that when i edit a file it will automatically compile everything

--- guitar-search-demo/Gruntfile.js
+++ guitar-search-demo/Gruntfile.js
@@ -1,105 +1,122 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	concat: {
-	  options: {
-		separator: ';'
-	  },
-	  dist: {       	 
-		  src: ['public/libs/angular/*.js',
-			  'public/libs/angular-route/*.js',
-			  'public/libs/angular-resource/*.js',
-			  'public/libs/angular-animate/*.js',
-			  'public/libs/jquery/*.js',
-			  'public/libs/underscore/*.js',
-			  'public/libs/bootstrap/*.js',
-			  'public/libs/angular-bootstrap/*.js',
-			  'public/js/**/*.js',
-			  'config/*.js'
-		],	 
-		dest: 'dist/<%= pkg.name %>.js'
-	  }
-	},
-	uglify: {
-	  options: {
-		  bare_returns: true,
-		banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-	  },
-	  dist: {
-		files: {
-		  'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-		}
-	  }	  
-	},
-	less: {
-	    // production config is also available
-	    development: {
-	        options: {
-	            // Specifies directories to scan for @import directives when parsing. 
-	            // Default value is the directory of the source, which is probably what you want.
-	            paths: ["public/css/", "public/libs/lesshat/build"],
-	        },
-	        files: {
-	            // compilation.css  :  source.less
-	            "public/css/style.css": "public/css/styles.less"
-	        }
-	    },
-	},
-	//qunit: {
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {       	 
+          src: ['public/libs/angular/*.js',
+              'public/libs/angular-route/*.js',
+              'public/libs/angular-resource/*.js',
+              'public/libs/angular-animate/*.js',
+              'public/libs/jquery/*.js',
+              'public/libs/underscore/*.js',
+              'public/libs/bootstrap/*.js',
+              'public/libs/angular-bootstrap/*.js',
+              'public/js/**/*.js',
+              'config/*.js'
+        ],	 
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+          bare_returns: true,
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    less: {
+        // production config is also available
+        dev: {
+            options: {
+                // Specifies directories to scan for @import directives when parsing. 
+                // Default value is the directory of the source, which is probably what you want.
+                paths: ["public/css/"],
+            },
+            files: {
+                // compilation.css  :  source.less
+                "public/css/style.css": "public/css/styles.less"
+            }
+        },
+    },
+    //qunit: {
 //      files: ['public/**/*.html']
   //  },
-	jshint: {
-	  files: ['Gruntfile.js', 'public/js/**/*.js','config/*.js', 'app/*.js'],
-	  options: {
-		// options here to override JSHint defaults
-		globals: {
-		  jQuery: true,
-		  console: true,
-		  module: true,
-		  document: true
-		}
-	  }
-	},
-	watch: {
-	  files: ['<%= jshint.files %>'],
-	  tasks: ['jshint']//, 'qunit']
-	},
-	karma: {
-		unit: {
-			options: {
-				colors: true,				
-				frameworks: ['jasmine'],
-				singleRun: true,
-				browsers: ['PhantomJS'],
-				files: [
-				'public/libs/angular/angular.js',
-				'public/libs/angular-mocks/angular-mocks.js',
-				'public/libs/angular-route/angular-route.js',
-				'public/libs/angular-resource/angular-resource.js',
-				'public/libs/underscore/underscore.js',
-				'public/js/**/*.js',
-				'tests/**/*.js',
-				'config/*.js' ,
-				'public/libs/angular-bootstrap/ui-bootstrap.js'
-				],
-				plugins: [
-				'karma-phantomjs-launcher',
-				'karma-jasmine',
-				'karma-junit-reporter'
-				],
-				reporters: ['dots', 'junit'],
-				junitReporter: {
-					outputFile: '../test-results.xml'
-				}//,
-				//logLevel: 'LOG_DISABLE'
-			}
-		}
-	}
+    jshint: {
+      files: ['Gruntfile.js', 'public/js/**/*.js','config/*.js', 'app/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    //watch: {
+    //  files: ['<%= jshint.files %>'],
+      //  tasks: ['jshint']//, 'qunit']
+    watch: {
+        scripts: {
+            files: [
+                'public/js/*.js',
+                'tests/**/*.spec.js'
+            ],
+            tasks: ['default']
+        },
+        less: {
+            files: ['public/css/*.less'],
+            tasks: ['less:dev']
+        },
+        templates: {
+            files: [
+                'public/views/**/*.html'
+            ]
+        }
+    },
+    karma: {
+        unit: {
+            options: {
+                colors: true,
+                frameworks: ['jasmine'],
+                singleRun: true,
+                browsers: ['PhantomJS'],
+                files: [
+                'public/libs/angular/angular.js',
+                'public/libs/angular-mocks/angular-mocks.js',
+                'public/libs/angular-route/angular-route.js',
+                'public/libs/angular-resource/angular-resource.js',
+                'public/libs/underscore/underscore.js',
+                'public/js/**/*.js',
+                'tests/**/*.js',
+                'config/*.js' ,
+                'public/libs/angular-bootstrap/ui-bootstrap.js'
+                ],
+                plugins: [
+                'karma-phantomjs-launcher',
+                'karma-jasmine',
+                'karma-junit-reporter'
+                ],
+                reporters: ['dots', 'junit'],
+                junitReporter: {
+                    outputFile: '../test-results.xml'
+                },
+                logLevel: 'LOG_DISABLE'
+            }
+        }
+    }
   });
 
   //if (grunt.option('debug')){
-	// console.log(grunt.config('jshint.files'));
+    // console.log(grunt.config('jshint.files'));
   //}
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -109,9 +126,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-karma');
   // grunt.loadNpmTasks('karma-jasmine');
-	  
- // grunt.registerTask('test', ['jshint']);//, 'qunit']);
+  // grunt.registerTask('test', ['jshint']);//, 'qunit']);
 
-  grunt.registerTask('default', ['jshint', 'karma', 'concat', 'less', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'karma', 'concat', 'less', 'less:dev', 'uglify']);
+  grunt.registerTask('dev', ['default', 'watch']);
 
 };
